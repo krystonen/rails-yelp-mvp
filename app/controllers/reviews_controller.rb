c9fe5cf391afe543bce5727ff7ade2ce514c3530
Krisztina Kovari <krystonen@gmail.com>
2018-10-31 14:47:17 +0100

    Solution to rails

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,31 @@
+class ReviewsController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+    def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.restaurant = Restaurant.find(params[:restaurant_id])
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+end
+
+
+# /restaurants/:restaurant_id/reviews/new  reviews#new
+# restaurant_reviews  POST  /restaurants/:restaurant_id/reviews      reviews#create
---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true
+  validates :category, acceptance: { accept: ["chinese", "italian", "japanese", "french", "belgian"]}
+end
